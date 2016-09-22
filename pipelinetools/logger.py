04eab42b0a5aa811182080da7f006756245b26f0
ekhaydarov <erkin.khaydarov@holidayextras.com>
2016-09-22 21:14:14 +0100

    working directory debug for dataflow

--- pipelinetools/logger.py
+++ pipelinetools/logger.py
@@ -3,12 +3,16 @@ import os
 from ConfigParser import ConfigParser
 
 WORKING_DIR = os.path.abspath(os.path.dirname(__file__))
+print(os.getcwdu())
+print(os.path.dirname(os.path.realpath(__file__)))
 
 if __name__ != '__main__':
     import __main__
-    WORKING_DIR = os.path.abspath(os.path.dirname(__main__.__file__))
-    if 'unittest' in WORKING_DIR:
-        WORKING_DIR = os.path.abspath(os.path.dirname(__file__))
+    if '__file__' in dir(__main__):
+        WORKING_DIR = os.path.abspath(os.path.dirname(__main__.__file__))
+        if 'unittest' in WORKING_DIR:
+            WORKING_DIR = os.path.abspath(os.path.dirname(__file__))
+print(WORKING_DIR)
 CONFIG_FILE = WORKING_DIR + "/config"
 
 #  to enable testing we should make a dynamic path available
