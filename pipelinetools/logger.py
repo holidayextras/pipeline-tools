ca09c0f869d570af8d640cc322bafe68fc273780
ekhaydarov <erkin.khaydarov@holidayextras.com>
2016-09-22 22:06:12 +0100

    correct paths for dataflow to read config

--- pipelinetools/logger.py
+++ pipelinetools/logger.py
@@ -3,16 +3,11 @@ import os
 from ConfigParser import ConfigParser
 
 WORKING_DIR = os.path.abspath(os.path.dirname(__file__))
-print(os.getcwdu())
-print(os.path.dirname(os.path.realpath(__file__)))
 
 if __name__ != '__main__':
     import __main__
-    if '__file__' in dir(__main__):
-        WORKING_DIR = os.path.abspath(os.path.dirname(__main__.__file__))
-        if 'unittest' in WORKING_DIR:
-            WORKING_DIR = os.path.abspath(os.path.dirname(__file__))
-print(WORKING_DIR)
+    if 'unittest' not in __main__.__file__:
+        WORKING_DIR = WORKING_DIR[:-14]
 CONFIG_FILE = WORKING_DIR + "/config"
 
 #  to enable testing we should make a dynamic path available
@@ -21,6 +16,9 @@ config.read(CONFIG_FILE)
 
 
 def init_a_logger():
+    print("-------->", WORKING_DIR)
+    print(os.getcwdu())
+    print(os.path.dirname(os.path.realpath(__file__)))
 
     log_name = config.get('override', 'log_name')
     log_level = config.get('override', 'log_level')
