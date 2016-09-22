ab03b6d7b3e5ce929b2250f456e671fedbaf250d
ekhaydarov <erkin.khaydarov@holidayextras.com>
2016-09-22 23:36:33 +0100

    more testing

--- pipelinetools/logger.py
+++ pipelinetools/logger.py
@@ -8,11 +8,9 @@ if __name__ != '__main__':
     import __main__
     print('----------> 2')
     if '__file__' in dir(__main__):
-        print('----------> 2')
-        if 'unittest' not in __main__.__file__:
-            print('no __file__ using os.getcwdu')
-            WORKING_DIR = os.getcwdu()
-CONFIG_FILE = os.getcwdu() + "/config"
+        print('no __file__ using os.getcwdu')
+        WORKING_DIR = os.getcwdu()
+CONFIG_FILE = WORKING_DIR + "/config"
 
 #  to enable testing we should make a dynamic path available
 config = ConfigParser()
@@ -20,9 +18,9 @@ config.read(CONFIG_FILE)
 
 
 def init_a_logger():
-    print("-------->", WORKING_DIR)
+    print("-------->", CONFIG_FILE)
     print(os.getcwdu())
-    print(os.path.dirname(os.path.realpath(__file__)))
+    print(WORKING_DIR)
 
     log_name = config.get('override', 'log_name')
     log_level = config.get('override', 'log_level')
