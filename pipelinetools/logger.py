92c2d4707a0171443743ddc14bdee1105990d978
ekhaydarov <erkin.khaydarov@holidayextras.com>
2016-09-26 10:36:25 +0100

    show files in directories

--- pipelinetools/logger.py
+++ pipelinetools/logger.py
@@ -7,9 +7,12 @@ WORKING_DIR = os.path.abspath(os.path.dirname(__file__))
 if __name__ != '__main__':
     import __main__
     print('----------> 2')
-    if '__file__' in dir(__main__):
-        print('no __file__ using os.getcwdu')
+    if '__file__' not in dir(__main__):
+        print('__file__ does not exist in __main__')
         WORKING_DIR = os.getcwdu()
+    else:
+        print('__main__.__file__ exists!')
+        WORKING_DIR = os.path.abspath(os.path.dirname(__main__.__file__))
 CONFIG_FILE = WORKING_DIR + "/config"
 
 #  to enable testing we should make a dynamic path available
@@ -19,8 +22,10 @@ config.read(CONFIG_FILE)
 
 def init_a_logger():
     print("-------->", CONFIG_FILE)
-    print(os.getcwdu())
-    print(WORKING_DIR)
+    print("files in getcwdu -------->", [content for content in os.listdir(
+        os.getcwdu())])
+    print("files in WORKING_DIR -------->", [content for content in os.listdir(
+        os.path.abspath(os.path.dirname(__file__)))])
 
     log_name = config.get('override', 'log_name')
     log_level = config.get('override', 'log_level')
