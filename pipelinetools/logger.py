94f6ed67bc3c5022c41d311ca05e4fdfd7eac90e
ekhaydarov <erkin.khaydarov@holidayextras.com>
2016-09-26 14:24:07 +0100

    make a check_config() to use local config if no __file__ in __main__

--- pipelinetools/logger.py
+++ pipelinetools/logger.py
@@ -2,37 +2,23 @@ import logging
 import os
 from ConfigParser import ConfigParser
 
-WORKING_DIR = os.path.abspath(os.path.dirname(__file__))
 
-if __name__ != '__main__':
-    import __main__
-    print('----------> 2')
-    if '__file__' not in dir(__main__):
-        print('__file__ does not exist in __main__')
-        WORKING_DIR = os.getcwdu()
-    else:
-        print('__main__.__file__ exists!')
-        WORKING_DIR = os.path.abspath(os.path.dirname(__main__.__file__))
-CONFIG_FILE = WORKING_DIR + "/config"
-
-#  to enable testing we should make a dynamic path available
-config = ConfigParser()
-config.read(CONFIG_FILE)
+def check_config():
+    WORKING_DIR = os.path.abspath(os.path.dirname(__file__))
+    CONFIG_FILE = WORKING_DIR + "/config"
+    if __name__ != '__main__':
+        import __main__
+        if '__file__' not in dir(__main__):
+            return CONFIG_FILE
+        elif ('unittest' in __main__.__file__) or ('pycharm' in __main__.__file__):
+            return CONFIG_FILE
+        else:
+            CONFIG_FILE = os.path.abspath(os.path.dirname(__main__.__file__))\
+                          + '/config'
+    return CONFIG_FILE
 
 
-def init_a_logger():
-    print("-------->", CONFIG_FILE)
-    print("files in getcwdu -------->", [content for content in os.listdir(
-        os.getcwdu())])
-    print("files in WORKING_DIR -------->", [content for content in os.listdir(
-        os.path.abspath(os.path.dirname(__file__)))])
-
-    log_name = config.get('override', 'log_name')
-    log_level = config.get('override', 'log_level')
-    # create logger
-    logger = logging.Logger(log_name)
-    logger.setLevel(log_level)
-
+def console_handler(log_level):
     # create console handler and set level to debug
     ch = logging.StreamHandler()
     ch.setLevel(log_level)
@@ -44,7 +30,28 @@ def init_a_logger():
     # add formatter to ch
     ch.setFormatter(formatter)
 
+    return ch
+
+CONFIG_FILE = check_config()
+
+#  to enable testing we should make a dynamic path available
+config = ConfigParser()
+config.read(CONFIG_FILE)
+
+
+def init_a_logger():
+
+    log_name = config.get('override', 'log_name')
+    log_level = config.get('override', 'log_level')
+    # create logger
+    logger = logging.Logger(log_name)
+    logger.setLevel(log_level)
+
+    ch = console_handler(log_level)
+
     # add ch to logger
     logger.addHandler(ch)
+
+    logger.debug('check')
 
     return logger
